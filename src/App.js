9e1adca68da26ff1e263f155437592452b234fcc
Rylan Cole <rylancole@Rylans-Air.hitronhub.home>
2020-08-17 11:10:05 -0700

    hooked up reddit api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import snoowrap from 'snoowrap';
+
+const r = new snoowrap({
+  userAgent: 'test',
+  clientId: 'wzqtMiLpg3TWnQ',
+  clientSecret: 'FrjcB9AgxblsERw8HHkJH_zuf_o',
+  refreshToken: '23524788-w2JO3W9Lo6O7-uIKHJ3DHg_PnAE'
+});
+
+function sumCoins(awards) {
+  let s = 0
+  for (const award of awards ) {
+    s += award.coin_price
+  } 
+  return s
+}
+
+function filterSubmission(sub) {
+  return ({
+    ready: true,
+    title: sub.title,
+    authorName: sub.author.name,
+    body: sub.body,
+    thumbnail: sub.thumbnail,
+    url: sub.url,
+    cost: sumCoins(sub.all_awardings)
+  });
+}
+
+function EmbededPost(props) {
+  const { title, authorName, url, cost } = props
+  return (
+    <div>
+      /u/{authorName} <br/>
+      <a href={url}>{title}</a> <br/>
+      Value = {cost} coins
+    </div>
+  )
+}
 
 function App() {
+  const [content, setContent] = React.useState({})
+  useEffect(() => {
+    r.getSubmission('iavm5h').fetch()
+    .then(submission => {
+      setContent(filterSubmission(submission))
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        (content.ready) 
+        ? <EmbededPost {...content}/>
+        : " Loading . . ." 
+      }
     </div>
   );
 }
